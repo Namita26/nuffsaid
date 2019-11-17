284f965933b729d6abf2782f811c78893e57b4c9
Namita <namitamaharanwar@gmail.com>
2019-11-17 22:39:58 +0530

    Add files via upload

---
+++ count_schools.py
@@ -0,0 +1,66 @@
+import csv
+
+
+def extract_file_contents():
+    """
+    Extracts the given CSV contents.
+    :return: list of CSV file rows.
+    """
+    list_of_rows = []
+    # provide the exact file path from your system.
+    with open('/Users/namitamaharanwar/Documents/sl051bai_mod1.csv', newline='') as data_file:
+        reader = csv.DictReader(data_file)
+        for row in reader:
+            list_of_rows.append(row)
+
+    return list_of_rows
+
+
+def print_counts():
+    """
+    Prints results of following questions:
+    How many total schools are in this data set?
+    How many schools are in each state?
+    How many schools are in each Metro-centric locale?
+    What city has the most schools in it? How many schools does it have in it?
+    How many unique cities have at least one school in it?
+    """
+    list_of_rows = []
+    metro_centric_locales = ['1', '2', '3', '4', '5', '6', '7', '8']
+    list_of_rows = extract_file_contents()
+
+    # How many total schools are in this data set?
+    list_of_schools = list(map(lambda x: x.get("SCHNAM05"), list_of_rows))
+    print("Total Schools: %s" % len(list_of_schools))
+
+    # How many schools are in each state?
+    list_of_states = set(list(map(lambda x: x.get("LSTATE05"), list_of_rows)))
+
+    print("Schools by State:")
+
+    for state in list_of_states:
+        print("%s: %s" % (state, len(list(filter(lambda x: x.get("LSTATE05") == state, list_of_rows)))))
+
+    # How many schools are in each Metro-centric locale?
+    print("Schools by Metro-centric locale:")
+    for mlocale in metro_centric_locales:
+        print("%s: %s" % (mlocale, len(list(filter(lambda x: x.get("MLOCALE") == mlocale, list_of_rows)))))
+
+    # What city has the most schools in it? How many schools does it have in it?
+    list_of_cities = set(list(map(lambda x: x.get("LCITY05"), list_of_rows)))
+    city_wise_schools = {}
+
+    for row in list_of_rows:
+        city = row.get("LCITY05")
+        if city in city_wise_schools.keys():
+            city_wise_schools[city] = city_wise_schools[city] + 1
+        else:
+            city_wise_schools[city] = 1
+
+    _sorted = sorted(city_wise_schools, key=city_wise_schools.get, reverse=True)
+    print("City with most schools: %s (%s schools)" % (_sorted[0], city_wise_schools[_sorted[0]]))
+    print("Unique cities with at least one school: %s" % len(list_of_cities))
+
+
+if __name__ == "__main__":
+    print_counts()
---
+++ search_schools.py
@@ -0,0 +1,133 @@
+import csv
+import datetime
+
+
+def extract_file_contents():
+    """
+    Extracts the given CSV contents.
+    :return: list of CSV file rows.
+    """
+    list_of_rows = []
+    # provide the exact file path from your system.
+    with open('/Users/namitamaharanwar/Documents/sl051bai_mod1.csv', newline='') as data_file:
+        reader = csv.DictReader(data_file)
+        for row in reader:
+            list_of_rows.append(row)
+
+    return list_of_rows
+
+
+def search_schools(search_key):
+    """
+    Searches the search key by cleaning and tokenizing the given string and matches with school names.
+    :param search_key: input school name
+    :return: prints the matching results with ranks
+    """
+    list_of_rows = extract_file_contents()
+    search_results_with_ranking = []
+    rank = 0
+
+    sub_strings = Tokenizer.sub_string_generator(search_key.lower())
+
+    for row in list_of_rows:
+
+        if Search.exact_match(search_key.lower(), row.get("SCHNAM05").lower()) != -1:
+            rank = rank + 1
+            search_results_with_ranking.append([rank, row.get("LCITY05"), row.get("LSTATE05"), row.get("SCHNAM05")])
+            break
+
+        for sub_string in sub_strings:
+            if Search.partial_match(sub_string, row.get("SCHNAM05").lower()) != -1:
+                rank = rank + 1
+                search_results_with_ranking.append([rank, row.get("LCITY05"), row.get("LSTATE05"), row.get("SCHNAM05")])
+
+    print("Results for '%s'" % search_key)
+    for row in search_results_with_ranking:
+        print("%s. %s" % (row[0], row[3]))
+        print("%s, %s" % (row[1], row[2]))
+
+
+class Search:
+
+    @staticmethod
+    def exact_match(search_key, source_string):
+        """
+        Checks whether search_key and source_string matches exactly.
+        :param search_key:
+        :param source_string:
+        :return: 1 if the string matched exactly and rank as 1 otherwise False
+        """
+        if search_key == source_string.lower():
+            return 1
+        return -1
+
+    @staticmethod
+    def partial_match(search_key, source_string):
+        """
+        Searches the entire search_key in the source_string
+        :param search_key:
+        :param source_string:
+        :return:
+        """
+        if search_key in source_string:
+            return 1
+        return -1
+
+
+class Tokenizer:
+
+    @staticmethod
+    def sub_string_generator(search_key):
+        """
+        Generates sub-stings based on the number of words in the search key.
+        :param search_key: Input string
+        :return: Array of dicts with sub-strings and their relevant rank.
+        """
+        search_key = Cleaner.remove_all_special_chars(search_key)
+        search_keys = search_key.split()
+        sub_strings = []
+
+        for i in range(len(search_keys)):
+            if i < len(search_keys)-1:
+                sub_strings.append(" ".join(search_keys[0:(len(search_keys) - (i+1))]))
+            elif i < len(search_keys)-1:
+                continue
+
+        return sub_strings
+
+
+class Cleaner:
+
+    @staticmethod
+    def remove_all_special_chars(search_key):
+        """
+        Removes all predefined special characters from the input string.
+        :param search_key: string to be searched
+        :return: Same search_key after removing all special chars
+        """
+        special_chars = ["%", "!", "@", "#", "$", "^", "&", "*", "(", ")", "_", "-", "+", "'", '"', ",", "."]
+        for char in special_chars:
+            search_key.replace(char, " ")
+
+        return search_key
+
+    @staticmethod
+    def digit_split(search_key):
+        """
+        Splits the given input string if any digit is encountered.
+        :param search_key: Input string
+        :return: Input string with digits / numbers separated
+        """
+        digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+        for digit in digits:
+            search_key.replace(digit, "")
+
+        return search_key
+
+
+if __name__ == "__main__":
+    x = datetime.datetime.now()
+    search_schools("jefferson belleville".lower())
+    y = datetime.datetime.now()
+    z = y-x
+    print(z)
